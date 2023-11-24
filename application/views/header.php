734dcba966dd233ffc83d4baeb762efd47da5595
Tomichette <tjstpl22@gmail.com>
2023-11-24 17:41:37 +0100

    Fin Seance 1

--- application/views/header.php
+++ application/views/header.php
@@ -4,8 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dicatlon</title>
+    <link rel="stylesheet" href="style-header.css">
+    <link rel="stylesheet" href="style-boutique.css">
 </head>
 <body>
 <header>
- <h1>TOTOTOT</h1>
+        <div class="navbar">
+            <div class="accueil-icon">
+                <img src="img/Logo-Dicatlon.png" href="index.php"/>
+            </div>
+            <div class="menu">
+                <div class="pannier-container">
+                    <img src="img/btn-Panier.png" href="index.php"/>
+                </div>
+                <div class="compte-container">
+                    <img src="img/btn-Profile.png" href="index.php"/>
+                </div>
+            </div>
+        </div>
 </header>
