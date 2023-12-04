caf444ee41f15197e45264e7c41ca37ce4417ff1
Gabin11133 <gabincastelain@gmail.com>
2023-12-04 14:32:05 +0100

    page boutique

--- application/views/index.php
+++ application/views/index.php
@@ -1,7 +1,43 @@
-<?php
-require_once('header.php')
-?>
-    <h1>tatata</h1>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="shortcut icon" href="" type="img">
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="style-boutique.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boutique Dicathlon</title>
+</head>
+<body>
+    <div class="burger">
+        <img class="img_logo" src="img/Logo-Dicatlon.png" alt="image logo Dicathlon"/>
+        <img class="img_burger" src="img/burger.png" alt="burger de recherche"/>
+    </div>
+
+    <div class="titre">
+        <h1>Nos appareil de fitness en location</h1>
+    </div>
+
+    <div class="box_boutique">
+
+        <div class="item">
+            <div class="image">
+                <img class="velo_decath" src="img/velo_decath.png" alt="image velo decathlon"/>
+            </div>
+            <div class="description">
+                <div class="text">
+                    <p1>VÉLO ELLIPTIQUE EL540</p1>
+                    <p2>CONNECTÉ</p2>
+                    <p3>35€ par mois</p3>
+                </div>
+                <div class="bouton_panier">
+                    <img class="panier" src="img/panier.png" alt="bouton panier"/>
+                </div>
+            </div>
+        </div>
+
+    </div>
     
 </body>
 </html>
