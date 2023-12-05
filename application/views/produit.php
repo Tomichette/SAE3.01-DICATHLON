c8fbd97f99ce87f76ab6fc2be22c600ad7a90aa3
Tomichette <tjstpl22@gmail.com>
2023-12-05 12:11:30 +0100

    page produit

--- application/views/produit.php
+++ application/views/produit.php
@@ -7,6 +7,33 @@
     <link rel="stylesheet" href="<?php echo(base_url());?>css/style-details.css">
 </head>
 <body>
+    <div class="produit-container">
+        <div class="img-container">
+            <div class="images-produit-container-tete">
+                <img class="img-produit-1" src="<?php echo(base_url());?>img/velo-img1.jpg" alt="image produit"/>
+            </div>
+            <div class="images-produit-container-bas">
+                <img class="img-produit-2" src="<?php echo(base_url());?>img/velo-img2.jpg" alt="image produit"/>
+                <img class="img-produit-3" src="<?php echo(base_url());?>img/velo-img3.jpg" alt="image produit"/>
+                <img class="img-produit-4" src="<?php echo(base_url());?>img/velo-img4.jpg" alt="image produit"/>
+            </div>
+        <div>
+        <div class="info-produit">
+            <div class="titre-produit">
+                <h1>VÉLO ELLIPTIQUE EL540 CONNECTÉ</h1>
+                <h2>35,00 €/mois</h2>
+            </div>
+            <div class="texte">
+                <a>L'abonnement de ce vélo elliptique comprend :</a>
+                <ul>
+                    <li>La livraison à domicile & retour : notre service vous livre votre produit devant chez vous non monté (au pas de porte/au pied d'immeuble) et reprend le produit dans sa pièce d'utilisation</li>
+                    <li>Vivre une expérience connectée avec l'application gratuite E connected qui vous accompagne avec des programmes d'entrainement pour vous aider à progresser pour atteindre vos objectifs.</li>
+                    <li>Un SAV réactif en cas de pépin : Decathlon et Domyos portent une attention toute particulière au suivi de nos produits. Nous gérons nous même le service après-vente de nos produits et nous assurons la disponibilité des pièces détachées.</li>
+                </ul>
+                <img class="img_logo" src="<?php echo(base_url());?>img/btn-louer" alt="btn louer"/>
+            </div>
+        </div>
+    </div>
 
     <script type = 'text/javascript' src = "<?php echo base_url(); 
    ?>js/sample.js"></script>
